fa5081de335dd864fca68a13ae5a1c11151a5e39
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-10-27 14:34:58 +0200

    Static list

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Quindi rivediamo l’esercizio visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
 // Consigli:
 // Come posso ciclare su un array con il ciclo while?
+
+const arrGroceryList = [
+    'Pasta',
+    'Carne',
+    'Pesce',
+    'Verdura',
+    'Frutta',
+];
+
+const eleGroceryList = document.querySelector ('.grocery-list');
+
+for (let i = 0; i < arrGroceryList.length; i++) {
+	eleGroceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
+}
